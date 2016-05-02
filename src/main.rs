75c73ef5474c4b84463006cbcc582fad912dc45b
Philip Jackson <p-jackson@live.com>
2016-05-02 23:24:57 +1200

    Fixed graphemes that don't fit in a single code point

--- src/main.rs
+++ src/main.rs
@@ -31,19 +31,29 @@ fn main() {
     .and_then(|d| d.help(true).version(Some("v0.1.0".to_string())).decode())
     .unwrap_or_else(|e| e.exit());
 
-  if let Some(as_int) = first_grapheme_as_int(&args.arg_grapheme) {
-    println!("css  = {}", as_css(as_int));
-    println!("html = {}", as_html(as_int));
-    println!("js   = {}", as_js(as_int));
-  }
+  let grapheme = args.arg_grapheme;
+
+  println!(r#"css  = "{}""#, escape_grapheme(&grapheme, as_css));
+  println!(r#"html = {}"#, escape_grapheme(&grapheme, as_html));
+  println!(r#"js   = "{}""#, escape_grapheme(&grapheme, as_js));
 }
 
-fn first_grapheme_as_int(grapheme: &str) -> Option<u32> {
-  grapheme.chars().next().map(|c| c as u32)
+fn escape_grapheme<F>(grapheme: &str, int_to_escape_sequence: F) -> String
+  where F: Fn(u32) -> String
+{
+  grapheme
+    .chars()
+    .map(|ch| ch as u32)
+    .map(int_to_escape_sequence)
+    .fold(String::new(), |c, s| {
+      let mut copy = c;
+      copy.push_str(&s);
+      copy
+    })
 }
 
 fn as_css(i: u32) -> String {
-  format!("\"\\{:01$X}\"", i, 4)
+  format!("\\{:01$X}", i, 4)
 }
 
 fn as_html(i: u32) -> String {
@@ -52,48 +62,39 @@ fn as_html(i: u32) -> String {
 
 fn as_js(i: u32) -> String {
   if i <= 0xFFFF {
-    format!("\"\\u{:01$X}\"", i, 4)
+    format!("\\u{:01$X}", i, 4)
   } else {
-    format!("\"\\u{{{:X}}}\"", i)
+    format!("\\u{{{:X}}}", i)
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::{first_grapheme_as_int, as_css, as_html, as_js};
-
-  #[test]
-  fn test_first_grapheme_as_int() {
-    assert!(first_grapheme_as_int("\u{00A7}") == Some(0xA7));
-    assert!(first_grapheme_as_int("beef") == Some(98));
-    assert!(first_grapheme_as_int("\u{10000}") == Some(0x10000));
-    assert!(first_grapheme_as_int("\u{10FFFF}") == Some(0x10FFFF));
-    assert!(first_grapheme_as_int("") == None);
-  }
+  use super::{as_css, as_html, as_js};
 
   #[test]
   fn test_as_css() {
-    assert!(as_css(0) == r#""\0000""#);
-    assert!(as_css(0xFFFF) == r#""\FFFF""#);
-    assert!(as_css(0xBEEF) != r#""\beef""#);
+    assert!(as_css(0) == r"\0000");
+    assert!(as_css(0xFFFF) == r"\FFFF");
+    assert!(as_css(0xBEEF) != r"\beef");
     //assert!(as_css(0x10000) == ?);
   }
 
   #[test]
   fn test_as_html() {
-    assert!(as_html(0) == r#"&#x0000;"#);
-    assert!(as_html(0xFFFF) == r#"&#xFFFF;"#);
-    assert!(as_html(0xBEEF) != r#"&#xbeef;"#);
+    assert!(as_html(0) == r"&#x0000;");
+    assert!(as_html(0xFFFF) == r"&#xFFFF;");
+    assert!(as_html(0xBEEF) != r"&#xbeef;");
     //assert!(as_html(0x10000) == ?);
   }
 
   #[test]
   fn test_as_js() {
-    assert!(as_js(0) == r#""\u0000""#);
-    assert!(as_js(0xFFFF) == r#""\uFFFF""#);
-    assert!(as_js(0xBEEF) != r#""\ubeef""#);
-    assert!(as_js(0x10000) == r#""\u{10000}""#);
-    assert!(as_js(0x10FFFF) == r#""\u{10FFFF}""#);
-    assert!(as_js(0xFFFFFFFF) == r#""\u{FFFFFFFF}""#);
+    assert!(as_js(0) == r"\u0000");
+    assert!(as_js(0xFFFF) == r"\uFFFF");
+    assert!(as_js(0xBEEF) != r"\ubeef");
+    assert!(as_js(0x10000) == r"\u{10000}");
+    assert!(as_js(0x10FFFF) == r"\u{10FFFF}");
+    assert!(as_js(0xFFFFFFFF) == r"\u{FFFFFFFF}");
   }
 }
