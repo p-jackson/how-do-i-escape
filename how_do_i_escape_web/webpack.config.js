1c58e0f261c5a0ba24821ae341d5014d96764d58
Philip Jackson <p-jackson@live.com>
2018-12-30 18:43:58 +1300

    Optimise build output for production

--- how_do_i_escape_web/webpack.config.js
+++ how_do_i_escape_web/webpack.config.js
@@ -2,11 +2,14 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const dist = path.resolve(__dirname, "dist");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-module.exports = {
+module.exports = (env, options) => ({
   entry: "./src/index.js",
   output: {
     path: dist,
@@ -19,17 +22,34 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin(),
+
     new HtmlWebpackPlugin({
-      template: "index.html"
+      template: "index.html",
+      inject: false,
+      minify:
+        options.mode !== "production"
+          ? false
+          : {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              useShortDoctype: true
+            }
     }),
 
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "crate")
     })
-  ]
-};
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()]
+  }
+});
